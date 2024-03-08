bdc3df6f870776a69f4048ce02a84e824b4f857e
xmomoex <moimorex@gmail.com>
2024-03-09 00:23:27 +0100

    s

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/usuarios', [UsuarioController::class, 'index'])->name('usuarios.index');
+Route::get('/usuarios/create', [UsuarioController::class, 'create'])->name('usuarios.create');
+Route::post('/usuarios', [UsuarioController::class, 'store'])->name('usuarios.store');
+Route::get('/usuarios/{id}', [UsuarioController::class, 'show'])->name('usuarios.show');
+Route::get('/usuarios/{id}/edit', [UsuarioController::class, 'edit'])->name('usuarios.edit');
+Route::put('/usuarios/{id}', [UsuarioController::class, 'update'])->name('usuarios.update');
+Route::delete('/usuarios/{id}', [UsuarioController::class, 'destroy'])->name('usuario.destroy');
+
+
+
+
+Route::get('/login', [LoginController::class, 'login'])->name('usuarios.login');
+
+
+Route::post('/validar-registro', [LoginController::class, 'register'])->name('validar-registro');
+
+Route::post('/inicia-sesion', [LoginController::class, 'login'])->name('inicio-sesion');
+
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
