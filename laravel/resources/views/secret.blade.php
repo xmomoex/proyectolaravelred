6c94c619db4ff2791d8de4f6f154684f4862701a
xmomoex <moimorex@gmail.com>
2024-03-09 13:40:03 +0100

    s

--- laravel/resources/views/secret.blade.php
+++ laravel/resources/views/secret.blade.php
@@ -3,7 +3,7 @@
     <header class="d-flex flex-wrap align-items-center
     justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
         <a href="" class="d-flex align-items-center col-md-3 mb-2 
-    mb-md-0 text-dark text-decoration-none">Pagina privada</a>
+    mb-md-0 text-dark text-decoration-none">Pagina privada del Usuario</a>
 
         <div class="col-md-3 text-end">
             <a href="{{route('logout')}}">
@@ -12,6 +12,6 @@
         </div>
     </header>
     <article class="container">
-        <h2>Tu seccion Privada</h2>
+        <h3>Seccion privada</h3>
     </article>
 </div>
