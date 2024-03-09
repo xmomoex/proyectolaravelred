e607b465c82728b4c0c6d3616924639e9caa4063
xmomoex <moimorex@gmail.com>
2024-03-09 11:47:29 +0100

    s

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My App</title>
-    <!-- Enlaces a Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
@@ -24,7 +23,6 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Usuarios</a>
                 </li>
-                <!-- Agrega más enlaces según sea necesario -->
             </ul>
         </div>
     </nav>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -18,13 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/usuarios', [UsuarioController::class, 'index'])->name('usuarios.index');
-Route::get('/usuarios/create', [UsuarioController::class, 'create'])->name('usuarios.create');
-Route::post('/usuarios', [UsuarioController::class, 'store'])->name('usuarios.store');
-Route::get('/usuarios/{id}', [UsuarioController::class, 'show'])->name('usuarios.show');
-Route::get('/usuarios/{id}/edit', [UsuarioController::class, 'edit'])->name('usuarios.edit');
-Route::put('/usuarios/{id}', [UsuarioController::class, 'update'])->name('usuarios.update');
-Route::delete('/usuarios/{id}', [UsuarioController::class, 'destroy'])->name('usuario.destroy');
+
+Route::resource('/usuarios', UsuarioController::class);
 
 
 Route::view('/login', "login")->name('login');
